049b95f873f45728b1fcc232e7ddc576fe755d45
Guilherme Lincoln <gui.vini.lincoln@gmail.com>
2020-04-14 17:21:56 -0300

    Tipo de Dados e Operadores

---
+++ Ruby/operadores.rb
@@ -0,0 +1,29 @@
+
+numero1 = 10 
+numero2 = 2
+total = 0
+
+#puts 'Informe o número 1:'
+#numero1= gets.chomp.to_i
+
+#puts 'Informe o número 2:'
+#numero2 = gets.chomp.to_i
+
+#total = numero1 + numero2
+#total = numero1 - numero2
+#total = numero1 * numero2
+total = numero1 / numero2
+puts total
+
+#Operadores de Compareção
+
+v1 = 10
+v2 = 11
+
+puts v1 > v2   #maior
+puts v1 < v2   #menor
+puts v1 >= v2  #maior ou igual
+puts v1 <= v2  #menor ou igual
+puts v1 == v2  #igual
+puts v1 != v2  #não igual
+puts  v1.eql?(v2) #igual
---
+++ Ruby/tipos.rb
@@ -0,0 +1,19 @@
+#-- Tipos Ruby--
+# O tipo de uma variável é definido no momento em que a mesma recebe o valor
+
+pruduto = 'Macbook Air'
+preco = 5999.99
+quantidade = 10
+disponivel = true
+
+puts pruduto
+puts preco
+puts quantidade
+puts disponivel
+
+quantidade ='9'
+
+puts pruduto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class

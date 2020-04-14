2722261236f5d9e695d145f872d5148a15ae3c19
Guilherme Lincoln <gui.vini.lincoln@gmail.com>
2020-04-14 18:19:50 -0300

    Metodos

---
+++ Ruby/metodos.rb
@@ -0,0 +1,21 @@
+
+
+def diga_ola(nome)
+    puts 'Olá! Aprendendo metodos...' + nome
+end
+
+def retorno_nome
+    'Guilherme'
+end
+
+
+def soma(v1 ,v2)
+    total = v1 + v2
+    total
+end
+
+puts diga_ola('Guilherme')
+puts retorno_nome
+
+res = soma(10,25)
+puts res

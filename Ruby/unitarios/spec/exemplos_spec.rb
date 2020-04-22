2121237dc5549a8c92430d2ec8e3a74ccfb054b1
Guilherme Lincoln <gui.vini.lincoln@gmail.com>
2020-04-21 22:12:41 -0300

    Meu primeiro projeto com Rspec

---
+++ Ruby/unitarios/spec/exemplos_spec.rb
@@ -0,0 +1,9 @@
+
+describe 'exemplo de suite' do
+
+    it 'exemplo de cenario' do
+        idade = 20
+        expect(idade).to eql 20  # Fazendo um assert para saber se idade é igual a 20
+    end
+
+end
